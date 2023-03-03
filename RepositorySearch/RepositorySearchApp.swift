d63d298caf999a76f28933b70c7726be990b4a4f
Md Milan Mia <milanaiub@gmail.com>
2023-03-04 02:43:29 +0900

    :tada: Initialize Project

---
+++ RepositorySearch/RepositorySearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepositorySearchApp.swift
+//  RepositorySearch
+//
+//  Created by Matrix on 2023/03/04.
+//
+
+import SwiftUI
+
+@main
+struct RepositorySearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
