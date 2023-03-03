2d9282edd06e0d7c623065e73b8021f504b4bbb9
Md Milan Mia <milanaiub@gmail.com>
2023-03-04 02:51:57 +0900

    :mag_right: : Added search screen

---
+++ RepositorySearch/Screens/SearchScreen/SearchViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewModel.swift
+//  RepositorySearch
+//
+//  Created by Matrix on 2023/03/04.
+//
+
+import Combine
+import Foundation
+
+class SearchViewModel: ObservableObject {
+    
+}
