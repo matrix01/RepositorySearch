59362512edbe18de360c520427c10154f6db8be0
Md Milan Mia <milanaiub@gmail.com>
2023-03-04 14:40:17 +0900

    :mag: Search api service & test file added

---
+++ RepositorySearch/Screens/SearchScreen/APIServices/SearchAPIService.swift
@@ -0,0 +1,16 @@
+//
+//  SearchApiService.swift
+//  RepositorySearch
+//
+//  Created by Matrix on 2023/03/04.
+//
+
+import Foundation
+
+protocol RepoSearchAPIUseCase {
+    
+}
+
+class SearchAPIService: RepoSearchAPIUseCase {
+    
+}
---
+++ RepositorySearchTests/SearchScreenTests/SearchApiServiceTests.swift
@@ -0,0 +1,22 @@
+//
+//  SearchApiServiceTests.swift
+//  RepositorySearchTests
+//
+//  Created by Matrix on 2023/03/04.
+//
+
+import XCTest
+@testable import RepositorySearch
+
+final class SearchApiServiceTests: XCTestCase {
+
+    let sut: SearchAPIService! = nil
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+}
