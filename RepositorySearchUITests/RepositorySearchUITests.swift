2c0ed5bfd234f70c016f4bd9286e3b2cec73dbd8
Md Milan Mia <milanaiub@gmail.com>
2023-03-06 21:40:09 +0900

    :white_check_mark: UI tests for search view and detail view

--- RepositorySearchUITests/RepositorySearchUITests.swift
+++ RepositorySearchUITests/RepositorySearchUITests.swift
@@ -22,12 +22,62 @@ final class RepositorySearchUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func test_search_empty_view() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let timeout = 2.0
+        let empty = app.staticTexts["Empty"]
+        XCTAssertTrue(empty.waitForExistence(timeout: timeout))
+        XCTAssertTrue(empty.exists)
+    }
+    
+    func test_search_field_created() {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let timeout = 2.0
+        
+        // Get the navigation bar and search bar from navigation bar
+        let searchNavigationBar = app.navigationBars["Search"]
+        let searchSearchField = searchNavigationBar.searchFields["Search"]
+        XCTAssertTrue(searchSearchField.waitForExistence(timeout: timeout))
+    }
+    
+    func test_search_field_input() {
+        let app = XCUIApplication()
+        app.launch()
+        let timeout = 2.0
+        
+        // Get the navigation bar and search bar from navigation bar
+        let searchNavigationBar = app.navigationBars["Search"]
+        let searchSearchField = searchNavigationBar.searchFields["Search"]
+        XCTAssertTrue(searchSearchField.waitForExistence(timeout: timeout))
+        
+        // Tap and start typing
+        searchSearchField.tap()
+        searchSearchField.typeText("Hello")
+       
+        let listItems = app.collectionViews.buttons
+        XCTAssertTrue(listItems.firstMatch.waitForExistence(timeout: timeout))
+        XCTAssertTrue(listItems.count > 0)
+        XCTAssertEqual(searchSearchField.value as! String, "Hello")
+    }
+    
+    func test_open_detail_view_on_select() {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let searchNavigationBar = app.navigationBars["Search"]
+        let searchSearchField = searchNavigationBar.searchFields["Search"]
+        searchSearchField.tap()
+        searchSearchField.typeText("Hello")
+        
+        app.collectionViews/*@START_MENU_TOKEN@*/.buttons["https://github.com/phonegap/phonegap-start"]/*[[".cells.buttons[\"https:\/\/github.com\/phonegap\/phonegap-start\"]",".buttons[\"https:\/\/github.com\/phonegap\/phonegap-start\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.navigationBars["Detail"].buttons["Search"].tap()
+        searchSearchField.tap()
+        searchNavigationBar.buttons["Cancel"].tap()
     }
 
     func testLaunchPerformance() throws {
