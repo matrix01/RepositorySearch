a53adb6be80b7069cd17bbc63adc8f397ba5ae00
Md Milan Mia <milanaiub@gmail.com>
2023-03-06 23:18:15 +0900

    :pencil: Update doc

--- RepositorySearch/Screens/SearchScreen/APIServices/SearchAPIService.swift
+++ RepositorySearch/Screens/SearchScreen/APIServices/SearchAPIService.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 
+/// Specific use case for Search screen
 protocol RepoSearchAPIUseCase {
     func fetchRepoList(for endPoint: SearchAPICall) async throws -> RepositoryModel
 }
 
+/// API service to retrieve data from Server for search screen
 class SearchAPIService: APIServcie, RepoSearchAPIUseCase {
     func fetchRepoList(for endPoint: SearchAPICall) async throws -> RepositoryModel {
         return try await fetch(endpoint: endPoint)
